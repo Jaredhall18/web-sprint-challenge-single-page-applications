9f01bb1dc1eeae808d3c9043cb7c12f038441db1
jaredhall18 <jaredhall18@gmail.com>
2021-09-17 13:34:08 -0600

    created formSchema. Form now allows input's and displays error message if a required field is missing inputs.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'; //To post data later?
 import { Route, Link, Switch } from 'react-router-dom';
 import * as yup from 'yup';
+import schema from './validation/formSchema'
 
 //import components
 import Home from './components/Home'
@@ -35,6 +36,41 @@ const [orders, setOrders] = useState(initialOrders)
   const [formErrors, setFormErrors] = useState(initialFormErrors) 
   const [disabled, setDisabled] = useState(initialDisabled)       
 
+  const postNewOrder = newOrder => {
+    axios.post('https://reqres.in/api/orders', newOrder)
+    .then(res => {
+      setOrders([res.data, ...orders])
+      setFormValues(initialFormValues);
+    }).catch(err => {
+      console.error(err);
+      setFormValues(initialFormValues);
+    })
+  }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size.trim(),
+      special: formValues.special.trim(),
+      toppings: ['chicken', 'meatballs', 'olives', 'cheese'].filter(topping => !!formValues[topping])
+    }
+    formSubmit(newOrder);
+  }
 
   return (
     <>
@@ -48,8 +84,8 @@ const [orders, setOrders] = useState(initialOrders)
        <Route path={'/pizza'}>
           <PizzaForm 
           values={formValues}
-          // change={inputChange}
-          // submit={formSubmit}
+          change={inputChange}
+          submit={formSubmit}
           disabled={disabled}
           errors={formErrors}
           />
--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,3 +1,21 @@
 import * as yup from 'yup'
 
-//This will be used for cypress testing later
+const formSchema = yup.object().shape({
+    name: yup
+        .string()
+        .trim()
+        .required('name must be at least 2 characters')
+        .min(2, 'name must be at least 2 characters'),
+    size: yup
+        .string()
+        .oneOf(['small', 'medium', 'large', 'family'], 'Size is required!'),
+    chicken: yup.boolean(),
+    meatballs: yup.boolean(),
+    olives: yup.boolean(),
+    cheese: yup.boolean(),
+    special: yup
+        .string()
+        .trim()
+})
+
+export default formSchema;
